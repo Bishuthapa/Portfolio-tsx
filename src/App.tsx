5fc3d24fcc348de22c4aa09cdf3bd930b1ed2fe1
unknown <thapabishu435@gmail.com>
2025-06-03 23:41:35 +0545

    intial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Blogs from './components/Blog/Blog'; // <-- Blogs is the parent
-import AI from './components/Contents/runCcode'; // <-- Child of Blogs
-import FutureComputer from './components/Contents/FutureOfComputer';
+import AI from './components/Blog/contents/FutureOfComputer';
+import FutureComputer from './components/Blog/contents/FutureOfComputer';
 import Footer from './components/Footer/Footer';
 import PrivacyPolicy from './components/Privacy policy/PrivacyPolicy';
 import Contacts from './components/Contacts/Contacts';
 import NotFound from './components/NotFound/NotFound';
 import Blank from './components/blank/blank';
-
+import Totalbackend from '/Contents/Totalbackend.mdx';
 function App() {
   return (
     <div className="bg-white text-black dark:bg-black dark:text-white min-h-screen transition-colors duration-200">
@@ -23,6 +23,7 @@ function App() {
         <Route path="/blog" element={<Blogs />}>
           <Route path="about-ai" element={<AI />} />
           <Route path="Future-of-computer" element={<FutureComputer />} />
+          <Route path="Backend-with-nodejs" element={<Totalbackend />} />
         </Route>
 
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,40 +1,52 @@
 import { NavLink, Outlet } from "react-router-dom"
-function Blogs(){
-  return(
-    
-  <div className="h-auto w-full mx-auto px-6 py-10 ">
-    <h1 className="text-4xl font-bold mb-6">Blogs</h1>
-     <ul>
-      <li>
-        <NavLink
-          to="about-ai"
-          className={({ isActive }) =>
-        isActive
-          ? "text-blue-600 transition-all duration-200"
-          : "hover:font-bold transition-all duration-200"
-          }
-        >
-          Challenges and Joys of Learning AI
-        </NavLink>
-      </li>
+function Blogs() {
+  return (
 
-      <li>
-        <NavLink
-          to="Future-of-computer"
-          className={({ isActive }) =>
-        isActive
-          ? "text-blue-600 transition-all duration-200"
-          : "hover:font-bold transition-all duration-200"
-          }
-        >
-          Future of Computer
-        </NavLink>
-      </li>
-     </ul>
-     <div>
-        <Outlet/>
-     </div>
-  </div>
+    <div className="h-auto w-full mx-auto px-6 py-10 ">
+      <h1 className="text-4xl font-bold mb-6">Blogs</h1>
+      <ul>
+        <li>
+          <NavLink
+            to="about-ai"
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-600 transition-all duration-200"
+                : "hover:font-bold transition-all duration-200"
+            }
+          >
+            Challenges and Joys of Learning AI
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="Future-of-computer"
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-600 transition-all duration-200"
+                : "hover:font-bold transition-all duration-200"
+            }
+          >
+            Future of Computer
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="backend-with-nodejs"
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-600 transition-all duration-200"
+                : "hover:font-bold transition-all duration-200"
+            }
+          >
+            Backend with Node.js
+          </NavLink>
+        </li>
+      </ul>
+      <div>
+        <Outlet />
+      </div>
+    </div>
 
   )
 
