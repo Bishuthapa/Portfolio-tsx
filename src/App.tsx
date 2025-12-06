11de326031c5f24543be3e91c40915824b9cfcdd
Bishu Thapa <thapabishu435@gmail.com>
2025-12-06 12:33:58 +0545

    refactor: remove unused Blog component and related content

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
 import About from './components/About/About';
-import Blogs from './components/Blog/Blog'; // <-- Blogs is the parent
-import AI from './components/Blog/contents/runCcode';
-import FutureComputer from './components/Blog/contents/FutureOfComputer';
 import Footer from './components/Footer/Footer';
 import PrivacyPolicy from './components/Privacy policy/PrivacyPolicy';
 import Contacts from './components/Contacts/Contacts';
 import NotFound from './components/NotFound/NotFound';
 import Blank from './components/blank/blank';
-import Totalbackend from '/Contents/Totalbackend.mdx';
 import { useState, createContext , useContext } from 'react';
 
 
@@ -47,12 +43,8 @@ export default function App() {
         <Route path="/about" element={<About />} />
 
         {/* NESTED BLOG ROUTES */}
-        <Route path="/blog" element={<Blogs />}>
-          <Route path="ai" element={<AI />} />
-          <Route path="Future-of-computer" element={<FutureComputer />} />
-          <Route path="Backend-with-nodejs" element={<Totalbackend />} />
-        </Route>
-
+        
+        
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="/blank" element={<Blank />} />
