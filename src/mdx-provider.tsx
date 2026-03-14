0dfe7216e6942020cc804d9e8b8a983fb56659ec
Bishuthapa <thapabishu435@gmail.com>
2026-03-14 11:15:41 +0545

    change

--- src/mdx-provider.tsx
+++
@@ -1,5 +0,0 @@
-import { MDXProvider } from '@mdx-js/react';
-
-export default function MdxProvider({ children }: { children: React.ReactNode }) {
-  return <MDXProvider>{children}</MDXProvider>;
-}
